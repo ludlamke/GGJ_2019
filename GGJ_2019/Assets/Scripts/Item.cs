48a26a8448d43bfcf1b7bc31b6fae4e16165f676
Lucas Freitas <lucasfreitas09876@gmail.com>
2019-01-26 12:49:00 -0500

    Item Script

--- GGJ_2019/Assets/Scripts/Item.cs
+++ GGJ_2019/Assets/Scripts/Item.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Item : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public int id;
+
+    float originY;
+
+    //Thank you  fafase on Unity Answers for the Sine animations
+        float amplitudeY = 0.06f;
+        float omegaY = 1.0f;
+        float index;
+
+    void Start() {
+        originY = transform.position.y;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+
+
+        //Animate item to make it stand out
+        //Again, thanks, fafase
+            index += Time.deltaTime;
+            float y = Mathf.Abs(amplitudeY * Mathf.Sin(omegaY * index));
+            transform.localPosition = new Vector2(transform.position.x, originY + y);
     }
 }

ecf93e269b0cffb85e8360fa6bd998cf4fc9deda
Kevin Ludlam <kevinlud92@gmail.com>
2019-01-25 23:26:57 -0500

    added some stuff

---
+++ GGJ_2019/Assets/Scripts/Invintory.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Invintory : MonoBehaviour
+{
+    public GameObject[] things;
+    public int number_of_things = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    public void getItem()
+    {
+      
+        number_of_things = number_of_things + 1;
+        things = new GameObject[number_of_things];
+    }
+}

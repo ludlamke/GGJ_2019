a2f22f90edf74ace3158fac314c740b4580544bc
Lucas Freitas <lucasfreitas09876@gmail.com>
2019-01-27 11:51:02 -0500

    camera

--- GGJ_2019/Assets/Scripts/CameraSystem.cs
+++ GGJ_2019/Assets/Scripts/CameraSystem.cs
@@ -4,51 +4,14 @@ using UnityEngine;
 
 public class CameraSystem : MonoBehaviour {
 
-    public float panSpeed;
     public float yOffset;
-    public float xOffset;
-
 
     public Transform target;
-    private Transform currentTrans;
+    private Vector3 currentPos;
 
-    public enum TrackModes { XLock, YLock, Pin};
-    public TrackModes trackMode;
+    private void Update() {
+        currentPos = new Vector3(target.position.x, transform.position.y + yOffset, -10);
 
-	// Use this for initialization
-	void Start () {
-        currentTrans = this.transform;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        switch (trackMode) {
-            case TrackModes.YLock:
-                TrackX();
-                break;
-            case TrackModes.XLock:
-                TrackY();
-                break;
-            case TrackModes.Pin:
-                TrackAll();
-                break;
-        }       
-    }
-
-    void TrackX() {
-        if(target.position.x > transform.position.x) {
-            //transform.position = new Vector3(target.position.x, currentTrans.position.y, currentTrans.position.z);
-            transform.Translate(Vector3.right * panSpeed * Time.deltaTime);
-        }
-    }
-
-    void TrackY() {
-        if (target.position.y > transform.position.y) {
-            transform.position = new Vector3(currentTrans.position.x, target.position.y, currentTrans.position.z);
-        }
-    }
-
-    void TrackAll() {
-        transform.position = new Vector3(target.position.x, target.position.y, currentTrans.position.z);
+        transform.position = currentPos;
     }
 }

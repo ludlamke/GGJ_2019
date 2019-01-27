765089934816a3106515d741b88a29b469e8fd79
Kevin Ludlam <kevinlud92@gmail.com>
2019-01-26 20:12:03 -0500

    stars update

---
+++ GGJ_2019/Assets/Scripts/stars.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+public class stars : MonoBehaviour
+{
+
+    public GameObject otherstars;
+    public GameObject player;
+    public Vector2 otherstarslocation;
+    public bool isusingstars;
+    public AudioClip walkStars;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.GetComponent<PlayerController>().use == true && isusingstars)
+        {
+            player.GetComponent<PlayerController>().use = false;
+            StartCoroutine(WaitForreset());
+            player.transform.position = otherstarslocation;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            isusingstars = true;
+            player = other.gameObject;
+            otherstarslocation = otherstars.transform.position;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            isusingstars = true;
+            player = null;
+            otherstarslocation = gameObject.transform.position;
+        }
+    }
+
+    IEnumerator WaitForreset()
+    {
+        
+        yield return new WaitForSeconds(1);
+        
+    }
+}

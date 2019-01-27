4d794bc0799b7b9ecf59c6698cd9ad8db54e38f2
Lucas Freitas <lucasfreitas09876@gmail.com>
2019-01-27 10:56:40 -0500

    built level layout

---
+++ GGJ_2019/Assets/Scripts/CameraSystem.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSystem : MonoBehaviour {
+
+    public float panSpeed;
+    public float yOffset;
+    public float xOffset;
+
+
+    public Transform target;
+    private Transform currentTrans;
+
+    public enum TrackModes { XLock, YLock, Pin};
+    public TrackModes trackMode;
+
+	// Use this for initialization
+	void Start () {
+        currentTrans = this.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        switch (trackMode) {
+            case TrackModes.YLock:
+                TrackX();
+                break;
+            case TrackModes.XLock:
+                TrackY();
+                break;
+            case TrackModes.Pin:
+                TrackAll();
+                break;
+        }       
+    }
+
+    void TrackX() {
+        if(target.position.x > transform.position.x) {
+            //transform.position = new Vector3(target.position.x, currentTrans.position.y, currentTrans.position.z);
+            transform.Translate(Vector3.right * panSpeed * Time.deltaTime);
+        }
+    }
+
+    void TrackY() {
+        if (target.position.y > transform.position.y) {
+            transform.position = new Vector3(currentTrans.position.x, target.position.y, currentTrans.position.z);
+        }
+    }
+
+    void TrackAll() {
+        transform.position = new Vector3(target.position.x, target.position.y, currentTrans.position.z);
+    }
+}
